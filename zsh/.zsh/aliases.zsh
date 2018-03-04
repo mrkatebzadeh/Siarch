c29eca2ed5e6fa6f99f677a90912a8c9c676357a
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-03-04 00:53:32 +0000

    Add more aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,7 +2,11 @@ alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} --exclude="
 alias h=history
 alias rm='rm -i'
 alias nano='nano -c'
+alias top=htop
+alias emacs=emacs -nw
+alias ll='ls -al'
 
 # suffix aliases http://zshwiki.org/home/examples/aliassuffix
 alias -s log=nano
 alias -s conf=nano
+
