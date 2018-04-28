f063a47daf9c5e18068d0b4fc9c1f1df9dd1dab3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2018-04-28 22:30:02 +0100

    Change default vim repo

--- _vim/installer.sh
+++ _vim/installer.sh
@@ -1,2 +1,2 @@
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+git clone --depth=1 https://github.com/mrkatebzadeh/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
