ab0b7736ad95780aa1d5dc9a617017f91f18e31e
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-01-09 02:09:45 +0000

    Move .doom.d to .config/doom

--- orgs/emacs/postscript.sh
+++ orgs/emacs/postscript.sh
@@ -6,6 +6,6 @@ PACKAGE=$2
 if [ ! -d ~/.emacs.d ]; then
     git clone -b develop https://github.com/hlissner/doom-emacs ~/.emacs.d
 fi
-mkdir -p ~/.doom.d/lisp
-cp -r ./lisp/* ~/.doom.d/lisp
+mkdir -p ~/.config/doom/lisp
+cp -r ./lisp/* ~/.config/doom/lisp
 ~/.emacs.d/bin/doom refresh -y
