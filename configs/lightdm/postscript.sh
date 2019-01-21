a4c500c12dd83f2e0239f567ab8ec939ada1a3fc
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-01-21 00:45:51 +0000

     Added lightdm
    - Installed lightdm
    - Install Litarvan theme
    - Added Obito's background

---
+++ configs/lightdm/postscript.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo cp ./lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp ./lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo cp ./background.jpg /usr/share/lightdm-webkit/themes/litarvan/images
