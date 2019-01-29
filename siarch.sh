1bfeee24a2708382a611d24ea3d065616b0dd4d4
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-01-29 12:29:26 +0000

    Added sudo to installation commands

--- siarch.sh
+++ siarch.sh
@@ -193,7 +193,7 @@ gitmakeinstall() {
 	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
 	cd "$dir" || exit
 	make >/dev/null 2>&1
-	make install >/dev/null 2>&1
+	sudo make install >/dev/null 2>&1
 	cd /tmp || return
 }
 
