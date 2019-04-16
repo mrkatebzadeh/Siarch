7049734d48224f8e929e37b7d00cff25b3c4b1eb
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-04-16 06:18:15 +0100

    Spacemacs is fast enough right now. Let's give it a try!

--- orgs/emacs/postscript.sh
+++ orgs/emacs/postscript.sh
@@ -4,8 +4,5 @@ DIRECTORY=$1
 PACKAGE=$2
 
 if [ ! -d ~/.emacs.d ]; then
-    git clone -b develop https://github.com/hlissner/doom-emacs ~/.emacs.d
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
-mkdir -p ~/.config/doom/lisp
-cp -r ./lisp/* ~/.config/doom/lisp
-~/.emacs.d/bin/doom -y refresh
