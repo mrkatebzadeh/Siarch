0b6e5742d7108b330474d0360ae8bc65c6993738
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2021-03-25 06:50:18 +0000

    C-f follows links

--- surf/.config/surf/scripts/followlink.js
+++ surf/.config/surf/scripts/followlink.js
@@ -169,8 +169,8 @@ document.addEventListener( 'keydown', initKeyBind, false );
 function initKeyBind(e){
 	var t = e.target;
 	if( t.nodeType == 1){
-		addKeyBind( 'f', 'hintMode()', e );
-		addKeyBind( 'F', 'hintMode(true)', e );
+		addKeyBind( 'C-f', 'hintMode()', e );
+		addKeyBind( 'C-F', 'hintMode(true)', e );
 		addKeyBind( 'C-c', 'removeHints()', e );
 	}
 }
