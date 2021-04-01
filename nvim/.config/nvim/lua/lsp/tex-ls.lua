f74f8469428b4781f4aa892362d135e8360fbd8f
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2021-04-01 01:22:54 +0100

    Fixed LSP for latex

--- nvim/.config/nvim/lua/lsp/tex-ls.lua
+++ nvim/.config/nvim/lua/lsp/tex-ls.lua
@@ -1,4 +1,5 @@
 require'lspconfig'.texlab.setup{
-    cmd = {DATA_PATH .. "/lspinstall/latex/texlab", "--stdio"},
+    cmd = {DATA_PATH .. "/lspinstall/latex/texlab"},
+	filetypes = {"plaintex", "tex", "bib"},
     on_attach = require'lsp'.common_on_attach,
 }
