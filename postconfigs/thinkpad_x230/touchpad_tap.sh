73dedd85c971523eaedd3048490378bfac7d2309
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2022-01-18 20:13:20 +0000

    Added tap to click

---
+++ postconfigs/thinkpad_x230/touchpad_tap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Tap to click
+[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        # Enable left mouse button by tapping
+        Option "Tapping" "on"
+EndSection%' > /etc/X11/xorg.conf.d/40-libinput.conf
