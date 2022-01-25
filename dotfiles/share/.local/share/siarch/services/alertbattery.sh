de7a760e14755a92f69cbda5d7245e3861c73e93
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2022-01-25 10:44:15 +0000

    5 seconds slack

--- dotfiles/share/.local/share/siarch/services/alertbattery.sh
+++ dotfiles/share/.local/share/siarch/services/alertbattery.sh
@@ -7,8 +7,9 @@ if [[ ${bat_status}=="Discharging" && ${capacity} -le 5 ]]; then
     echo "Battery alert - ${capacity}%"
     notify-send -u critical \
         --icon=$HOME/.local/share/siarch/icons/empty-battery-64.png \
-        "Hibernating ... " \
+        "Hibernating ... in 5 seconds" \
         "${capacity}% battery remaining"
+    sleep 5
     systemctl suspend-then-hibernate
 elif [[ ${bat_status}=="Discharging" && ${capacity} -le 35 ]]; then
     echo "Battery alert - ${capacity}%"
