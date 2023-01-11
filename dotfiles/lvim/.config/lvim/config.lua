6224db567cfd744d94c43d6305675b98ba821394
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2023-01-11 13:08:26 +0000

    Better config

--- dotfiles/lvim/.config/lvim/config.lua
+++ dotfiles/lvim/.config/lvim/config.lua
@@ -5,8 +5,8 @@ lvim is the global options object
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "xresources"
-vim.g.vimtex_view_method = 'zathura'
+lvim.colorscheme = "lunar"
+vim.g.vimtex_view_method = "zathura"
 vim.wo.relativenumber = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -56,50 +56,50 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 --   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 -- }
 lvim.builtin.which_key.mappings["!"] = lvim.builtin.which_key.mappings["L"]
-lvim.builtin.which_key.mappings["b"].d = { ':bp<bar>sp<bar>bn<bar>bd<CR>', 'Close Buffer' }
+lvim.builtin.which_key.mappings["b"].d = { ":bp<bar>sp<bar>bn<bar>bd<CR>", "Close Buffer" }
 lvim.builtin.which_key.mappings["f"] = {
-  name = "+Files",
-  f = {
-    function()
-      require("lvim.core.telescope.custom-finders").find_project_files { previewer = false }
-    end,
-    "Find File",
-  },
-  n = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
-  N = { ':NvimTreeOpen<CR>', 'Toggle Focus' },
-  F = { ':NvimTreeFindFile<CR>', 'Tree Find' },
-  w = { ':write<CR>', 'Write File' },
-  e = { ':e ', 'e' },
-  m = { ':Move', 'Move' },
-  d = { ':Delete<CR>', 'Delete' },
-  r = { ':Rename', 'Rename' },
-  c = { ':Chmod', 'Chmod' },
-  k = { ':Mkdir', 'Mkdir' },
-  W = { ':SudoWrite<CR>', 'SudoWrite' },
-  E = { ':SudoEdit', 'SudoEdit' },
+	name = "+Files",
+	f = {
+		function()
+			require("lvim.core.telescope.custom-finders").find_project_files({ previewer = false })
+		end,
+		"Find File",
+	},
+	n = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
+	N = { ":NvimTreeOpen<CR>", "Toggle Focus" },
+	F = { ":NvimTreeFindFile<CR>", "Tree Find" },
+	w = { ":write<CR>", "Write File" },
+	e = { ":e ", "e" },
+	m = { ":Move", "Move" },
+	d = { ":Delete<CR>", "Delete" },
+	r = { ":Rename", "Rename" },
+	c = { ":Chmod", "Chmod" },
+	k = { ":Mkdir", "Mkdir" },
+	W = { ":SudoWrite<CR>", "SudoWrite" },
+	E = { ":SudoEdit", "SudoEdit" },
 }
 lvim.builtin.which_key.mappings["w"] = {
-  name = "+Window",
-  w = { '<C-W>w', 'other-window' },
-  d = { '<C-W>c', 'delete-window' },
-  ['-'] = { '<C-W>s', 'split-window-below' },
-  ['|'] = { '<C-W>v', 'split-window-right' },
-  ['2'] = { '<C-W>v', 'layout-double-columns' },
-  h = { '<C-W>h', 'window-left' },
-  j = { '<C-W>j', 'window-below' },
-  l = { '<C-W>l', 'window-right' },
-  k = { '<C-W>k', 'window-up' },
-  H = { '<C-W>5<', 'expand-window-left' },
-  J = { ':resize +5<CR>', 'expand-window-below' },
-  L = { '<C-W>5>', 'expand-window-right' },
-  K = { ':resize -5<CR>', 'expand-window-up' },
-  ['='] = { '<C-W>=', 'balance-window' },
-  s = { '<C-W>s', 'split-window-below' },
-  v = { '<C-W>v', 'split-window-below' },
+	name = "+Window",
+	w = { "<C-W>w", "other-window" },
+	d = { "<C-W>c", "delete-window" },
+	["-"] = { "<C-W>s", "split-window-below" },
+	["|"] = { "<C-W>v", "split-window-right" },
+	["2"] = { "<C-W>v", "layout-double-columns" },
+	h = { "<C-W>h", "window-left" },
+	j = { "<C-W>j", "window-below" },
+	l = { "<C-W>l", "window-right" },
+	k = { "<C-W>k", "window-up" },
+	H = { "<C-W>5<", "expand-window-left" },
+	J = { ":resize +5<CR>", "expand-window-below" },
+	L = { "<C-W>5>", "expand-window-right" },
+	K = { ":resize -5<CR>", "expand-window-up" },
+	["="] = { "<C-W>=", "balance-window" },
+	s = { "<C-W>s", "split-window-below" },
+	v = { "<C-W>v", "split-window-below" },
 }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Toggles",
-  w = { ':set wrap!<CR>', 'Soft Wrap Text' },
+	name = "+Toggles",
+	w = { ":set wrap!<CR>", "Soft Wrap Text" },
 }
 
 table.insert(lvim.builtin.nvimtree.setup.view.mappings.list, { key = { "<tab>" }, action = "preview", mode = "n" })
@@ -114,19 +114,19 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "yaml",
-  "latex",
-  "dart",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"yaml",
+	"latex",
+	"dart",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -136,8 +136,8 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- -- make sure server will always be installed even if the server is in skipped_servers list
 lvim.lsp.installer.setup.ensure_installed = {
-  "sumneko_lua",
-  -- "jsonls",
+	"sumneko_lua",
+	-- "jsonls",
 }
 -- -- change UI setting of `LspInstallInfo`
 -- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
@@ -174,197 +174,274 @@ lvim.lsp.installer.setup.ensure_installed = {
 -- end
 
 -- -- set additional linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "shellcheck", filetypes = { "sh" } },
-  --   { command = "flake8", filetypes = { "python" } },
-  --   {
-  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --     command = "shellcheck",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     extra_args = { "--severity", "warning" },
-  --   },
-  --   {
-  --     command = "codespell",
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "javascript", "python" },
-  --   },
-}
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "shellcheck", filetypes = { "sh" } },
+	--   { command = "flake8", filetypes = { "python" } },
+	--   {
+	--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+	--     command = "shellcheck",
+	--     ---@usage arguments to pass to the formatter
+	--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+	--     extra_args = { "--severity", "warning" },
+	--   },
+	--   {
+	--     command = "codespell",
+	--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+	--     filetypes = { "javascript", "python" },
+	--   },
+})
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "stylua", filetypes = { "lua" } },
-  { command = "shfmt", filetypes = { "sh" } },
-  { command = "dart_format", filetypes = { "dart" } },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "shfmt", filetypes = { "sh" } },
+	{ command = "dart_format", filetypes = { "dart" } },
+})
 
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
 local codelldb_adapter = {
-  type = "server",
-  port = "${port}",
-  executable = {
-    command = mason_path .. "bin/codelldb",
-    args = { "--port", "${port}" },
-    -- On windows you may have to uncomment this:
-    -- detached = false,
-  },
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = mason_path .. "bin/codelldb",
+		args = { "--port", "${port}" },
+		-- On windows you may have to uncomment this:
+		-- detached = false,
+	},
 }
 
 pcall(function()
-  require("rust-tools").setup {
-    tools = {
-      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-      reload_workspace_from_cargo_toml = true,
-      runnables = {
-        use_telescope = true,
-      },
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = false,
-        parameter_hints_prefix = "<-",
-        other_hints_prefix = "=>",
-        max_len_align = false,
-        max_len_align_padding = 1,
-        right_align = false,
-        right_align_padding = 7,
-        highlight = "Comment",
-      },
-      hover_actions = {
-        border = "rounded",
-      },
-      on_initialized = function()
-        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-          pattern = { "*.rs" },
-          callback = function()
-            local _, _ = pcall(vim.lsp.codelens.refresh)
-          end,
-        })
-      end,
-    },
-    dap = {
-      adapter = codelldb_adapter,
-    },
-    server = {
-      on_attach = function(client, bufnr)
-        require("lvim.lsp").common_on_attach(client, bufnr)
-        local rt = require "rust-tools"
-        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-      end,
+	require("rust-tools").setup({
+		tools = {
+			executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+			reload_workspace_from_cargo_toml = true,
+			runnables = {
+				use_telescope = true,
+			},
+			inlay_hints = {
+				auto = true,
+				only_current_line = false,
+				show_parameter_hints = false,
+				parameter_hints_prefix = "<-",
+				other_hints_prefix = "=>",
+				max_len_align = false,
+				max_len_align_padding = 1,
+				right_align = false,
+				right_align_padding = 7,
+				highlight = "Comment",
+			},
+			hover_actions = {
+				border = "rounded",
+			},
+			on_initialized = function()
+				vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+					pattern = { "*.rs" },
+					callback = function()
+						local _, _ = pcall(vim.lsp.codelens.refresh)
+					end,
+				})
+			end,
+		},
+		dap = {
+			adapter = codelldb_adapter,
+		},
+		server = {
+			on_attach = function(client, bufnr)
+				require("lvim.lsp").common_on_attach(client, bufnr)
+				local rt = require("rust-tools")
+				vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+			end,
 
-      capabilities = require("lvim.lsp").common_capabilities(),
-      settings = {
-        ["rust-analyzer"] = {
-          lens = {
-            enable = true,
-          },
-          checkOnSave = {
-            enable = true,
-            command = "clippy",
-          },
-        },
-      },
-    },
-  }
+			capabilities = require("lvim.lsp").common_capabilities(),
+			settings = {
+				["rust-analyzer"] = {
+					lens = {
+						enable = true,
+					},
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+					},
+				},
+			},
+		},
+	})
 end)
 
 lvim.builtin.dap.on_config_done = function(dap)
-  dap.adapters.codelldb = codelldb_adapter
-  dap.configurations.rust = {
-    {
-      name = "Launch file",
-      type = "codelldb",
-      request = "launch",
-      program = function()
-        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-      end,
-      cwd = "${workspaceFolder}",
-      stopOnEntry = false,
-    },
-  }
+	dap.adapters.codelldb = codelldb_adapter
+	dap.configurations.rust = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+		},
+	}
 end
 
 -- Additional Plugins
 lvim.plugins = {
-  {
-    "lervag/vimtex",
-    cmd = { "VimtexInfo", "VimtexCompile", "VimtexStop", "VimtexClean", "VimtexView" },
-  },
-  {
-    'nekonako/xresources-nvim'
-  },
-  {
-    'jackMort/ChatGPT.nvim',
-    config = function()
-      require("chatgpt").setup({
-        -- optional configuration
-      })
-    end,
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  },
-  "simrat39/rust-tools.nvim",
-  {
-    "saecki/crates.nvim",
-    tag = "v0.3.0",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("crates").setup {
-        null_ls = {
-          enabled = true,
-          name = "crates.nvim",
-        },
-        popup = {
-          border = "rounded",
-        },
-      }
-    end,
-  },
-  {
-    "j-hui/fidget.nvim",
-    config = function()
-      require("fidget").setup()
-    end,
-  },
-  {
-    "akinsho/flutter-tools.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("flutter-tools").setup {
-        widget_guides = {
-          enabled = true,
-        },
-        closing_tags = {
-          highlight = "ErrorMsg", -- highlight for the closing tag
-          prefix = ">", -- character to use for close tag e.g. > Widget
-          enabled = true -- set to false to disable
-        },
-        dev_log = {
-          enabled = true,
-          open_cmd = "tabedit", -- command to use to open the log buffer
-        },
-        dev_tools = {
-          autostart = false, -- autostart devtools server if not detected
-          auto_open_browser = false, -- Automatically opens devtools in the browser
-        },
-        outline = {
-          open_cmd = "30vnew", -- command to use to open the outline buffer
-          auto_open = false -- if true this will open the outline automatically when it is first populated
-        },
-        lsp = {
-          on_attach = require("lvim.lsp").common_on_attach,
-        },
-      }
-    end,
-    ft = "dart",
-  },
+	{
+		"lervag/vimtex",
+		cmd = { "VimtexInfo", "VimtexCompile", "VimtexStop", "VimtexClean", "VimtexView" },
+	},
+	{
+		"nekonako/xresources-nvim",
+	},
+	{
+		"jackMort/ChatGPT.nvim",
+		config = function()
+			require("chatgpt").setup({
+				-- optional configuration
+			})
+		end,
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	"simrat39/rust-tools.nvim",
+	{
+		"saecki/crates.nvim",
+		tag = "v0.3.0",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+				popup = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
+	{
+		"akinsho/flutter-tools.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("flutter-tools").setup({
+				widget_guides = {
+					enabled = true,
+				},
+				closing_tags = {
+					prefix = "// ", -- character to use for close tag e.g. > Widget
+					enabled = true, -- set to false to disable
+				},
+				dev_log = {
+					enabled = true,
+				},
+				dev_tools = {
+					autostart = false, -- autostart devtools server if not detected
+					auto_open_browser = false, -- Automatically opens devtools in the browser
+				},
+				outline = {
+					open_cmd = "30vnew", -- command to use to open the outline buffer
+					auto_open = false, -- if true this will open the outline automatically when it is first populated
+				},
+				lsp = {
+					on_attach = require("lvim.lsp").common_on_attach,
+				},
+			})
+		end,
+		ft = "dart",
+	},
+	{
+		"kevinhwang91/nvim-bqf",
+		event = { "BufRead", "BufNew" },
+		config = function()
+			require("bqf").setup({
+				auto_enable = true,
+				preview = {
+					win_height = 12,
+					win_vheight = 12,
+					delay_syntax = 80,
+					border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+				},
+				func_map = {
+					vsplit = "",
+					ptogglemode = "z,",
+					stoggleup = "",
+				},
+				filter = {
+					fzf = {
+						action_for = { ["ctrl-s"] = "split" },
+						extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+					},
+				},
+			})
+		end,
+	},
+	{
+		"andymass/vim-matchup",
+		event = "CursorMoved",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		event = "BufRead",
+		config = function()
+			vim.cmd("highlight default link gitblame SpecialComment")
+			vim.g.gitblame_enabled = 0
+		end,
+	},
+	{
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({
+				width = 120, -- Width of the floating window
+				height = 25, -- Height of the floating window
+				default_mappings = true, -- Bind default mappings
+				debug = false, -- Print debug information
+				opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+				post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+				-- You can use "default_mappings = true" setup option
+				-- Or explicitly set keybindings
+				-- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+				-- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+				-- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "VimEnter" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup({
+					plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+					},
+				})
+			end, 100)
+		end,
+	},
+
+	{ "zbirenbaum/copilot-cmp", after = { "copilot.lua", "nvim-cmp" } },
 }
+
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
