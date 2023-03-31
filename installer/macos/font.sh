03bbf4ee311cc2c36a75e9d7a6735d2368a1c6a3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2023-03-31 13:45:55 +0100

    Added fonts

---
+++ installer/macos/font.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew install --cask font-fontawesome
+brew install --cask font-liberation-nerd-font
+
--- installer/macos/install.sh
+++ installer/macos/install.sh
@@ -1,4 +1,4 @@
-#!#!#!/bin/sh
+#!/bin/sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
