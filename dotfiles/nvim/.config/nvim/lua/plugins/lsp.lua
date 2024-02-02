8b20affd45b031db63cdb71cada3664e86e36f99
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-02 14:12:31 +0000

    Added lsp signature to rust

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,83 +1,91 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      ensure_installed = { "clangd", "cmake", "rust_analyzer", "lua_ls", "texlab" },
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    autostart = true,
-    config = function()
-      require("lspconfig.ui.windows").default_options.border = "rounded"
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = { "clangd", "cmake", "rust_analyzer", "lua_ls", "texlab" },
+			auto_install = true,
+		},
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		autostart = true,
+		config = function()
+			require("lspconfig.ui.windows").default_options.border = "rounded"
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.texlab.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.texlab.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.cmake.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
 
-      -- Clangd
-      local clangd_flags = {
-        "--all-scopes-completion",
-        "--suggest-missing-includes",
-        "--background-index",
-        "--pch-storage=disk",
-        "--cross-file-rename",
-        "--log=info",
-        "--completion-style=detailed",
-        "--enable-config",      -- clangd 11+ supports reading from .clangd configuration file
-        "--clang-tidy",
-        "--offset-encoding=utf-16", --temporary fix for null-ls
-        -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
-        -- "--fallback-style=Google",
-        -- "--header-insertion=never",
-        -- "--query-driver=<list-of-white-listed-complers>"
-      }
+			-- Clangd
+			local clangd_flags = {
+				"--all-scopes-completion",
+				"--suggest-missing-includes",
+				"--background-index",
+				"--pch-storage=disk",
+				"--cross-file-rename",
+				"--log=info",
+				"--completion-style=detailed",
+				"--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+				"--clang-tidy",
+				"--offset-encoding=utf-16", --temporary fix for null-ls
+				-- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+				-- "--fallback-style=Google",
+				-- "--header-insertion=never",
+				-- "--query-driver=<list-of-white-listed-complers>"
+			}
 
-      local clangd_bin = "clangd"
+			local clangd_bin = "clangd"
 
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        virtual_text = true,
-        cmd = { clangd_bin, unpack(clangd_flags) },
-        init_options = {
-          clangdFileStatus = true,
-          clangdSemanticHighlighting = true,
-        },
-        filetypes = { "c", "h", "cpp", "cxx", "cc", "hpp" },
-        settings = {
-          ["clangd"] = {
-            ["compilationDatabasePath"] = "build",
-            ["fallbackFlags"] = { "-std=c++17" },
-          },
-        },
-      })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				virtual_text = true,
+				cmd = { clangd_bin, unpack(clangd_flags) },
+				init_options = {
+					clangdFileStatus = true,
+					clangdSemanticHighlighting = true,
+				},
+				filetypes = { "c", "h", "cpp", "cxx", "cc", "hpp" },
+				settings = {
+					["clangd"] = {
+						["compilationDatabasePath"] = "build",
+						["fallbackFlags"] = { "-std=c++17" },
+					},
+				},
+			})
 
-      require("lspconfig").cmake.setup({})
-    end,
-  },
+			require("lspconfig").cmake.setup({})
+		end,
+	},
 }
--- dotfiles/nvim/.config/nvim/lua/plugins/rust.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,64 +1,70 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4", -- Recommended
-    ft = { "rust" },
-    config = function()
-      local cmp = require("cmp")
-      vim.api.nvim_create_autocmd("BufRead", {
-        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-        pattern = "Cargo.toml",
-        callback = function()
-          cmp.setup.buffer({ sources = { { name = "crates" } } })
-        end,
-      })
-      vim.g.rustaceanvim = {
-        inlay_hints = {
-          auto = true,
-          only_current_line = false,
-          show_parameter_hints = false,
-          parameter_hints_prefix = "<-",
-          other_hints_prefix = "=> ",
-          max_len_align = false,
-          max_len_align_padding = 1,
-          right_align = false,
-          right_align_padding = 7,
-          highlight = "Comment",
-        },
-        hover_actions = {
-          border = {
-            { "╭", "FloatBorder" },
-            { "─", "FloatBorder" },
-            { "╮", "FloatBorder" },
-            { "│", "FloatBorder" },
-            { "╯", "FloatBorder" },
-            { "─", "FloatBorder" },
-            { "╰", "FloatBorder" },
-            { "│", "FloatBorder" },
-          },
-          auto_focus = true,
-        },
-        server = {
-          on_attach = function(client, bufnr)
-            require("lsp-inlayhints").on_attach(client, bufnr)
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "saecki/crates.nvim",
-    tag = "stable",
-    config = function()
-      require("crates").setup({
-        null_ls = {
-          enabled = true,
-          name = "crates.nvim",
-        },
-        popup = {
-          border = "rounded",
-        },
-      })
-    end,
-  },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		ft = { "rust" },
+		config = function()
+			local cmp = require("cmp")
+			vim.api.nvim_create_autocmd("BufRead", {
+				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+				pattern = "Cargo.toml",
+				callback = function()
+					cmp.setup.buffer({ sources = { { name = "crates" } } })
+				end,
+			})
+			vim.g.rustaceanvim = {
+				inlay_hints = {
+					auto = true,
+					only_current_line = false,
+					show_parameter_hints = false,
+					parameter_hints_prefix = "<-",
+					other_hints_prefix = "=> ",
+					max_len_align = false,
+					max_len_align_padding = 1,
+					right_align = false,
+					right_align_padding = 7,
+					highlight = "Comment",
+				},
+				hover_actions = {
+					border = {
+						{ "╭", "FloatBorder" },
+						{ "─", "FloatBorder" },
+						{ "╮", "FloatBorder" },
+						{ "│", "FloatBorder" },
+						{ "╯", "FloatBorder" },
+						{ "─", "FloatBorder" },
+						{ "╰", "FloatBorder" },
+						{ "│", "FloatBorder" },
+					},
+					auto_focus = true,
+				},
+				server = {
+					on_attach = function(client, bufnr)
+						require("lsp-inlayhints").on_attach(client, bufnr)
+						require("lsp_signature").on_attach({
+							bind = true, -- This is mandatory, otherwise border config won't get registered.
+							handler_opts = {
+								border = "rounded",
+							},
+						}, bufnr)
+					end,
+				},
+			}
+		end,
+	},
+	{
+		"saecki/crates.nvim",
+		tag = "stable",
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+				popup = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
 }
