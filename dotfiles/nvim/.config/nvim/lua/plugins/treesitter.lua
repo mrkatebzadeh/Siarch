7c8ab2f9cb7187462480aebf77966f3fc136be27
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-06 12:06:25 +0000

    Added bash to treesitter

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				ensure_installed = { "c", "lua", "rust", "bash", "vim", "html" },
+				ensure_installed = { "c", "lua", "rust", "bash", "vim", "html", "bash" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
