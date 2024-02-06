480c3375da95d132ef3fc2e6d8e9a5578d498888
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-06 11:55:38 +0000

    Added barbar

--- dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
-	{
-		"hedyhli/outline.nvim",
-		config = function()
-			-- Example mapping to toggle outline
-			vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim",  -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {},
+    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
-			require("outline").setup({
-				-- Your setup opts here (leave empty to use defaults)
-			})
-		end,
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
-	},
+      require("outline").setup({
+        -- Your setup opts here (leave empty to use defaults)
+      })
+    end,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+  },
 }
