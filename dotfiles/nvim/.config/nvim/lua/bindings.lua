d06a5553bd1acf13b75521b9e4d79ab79b17a9aa
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-06 16:58:54 +0000

    Removed unnecessary line

--- dotfiles/nvim/.config/nvim/lua/bindings.lua
+++ dotfiles/nvim/.config/nvim/lua/bindings.lua
@@ -266,14 +266,6 @@ local mappings = {
 which_key.setup(setup)
 which_key.register(mappings, opts)
 
-opts = {
-  mode = "v",    -- NORMAL mode
-  prefix = "<leader>",
-  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}
 local K = vim.keymap.set
 K("n", "K", vim.lsp.buf.hover, {})
 
