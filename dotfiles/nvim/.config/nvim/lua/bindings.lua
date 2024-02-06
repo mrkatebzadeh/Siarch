d97f194f6403a29ce7c49acd35f8a2e9d78538cc
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-06 18:58:59 +0000

    Added surround

--- dotfiles/nvim/.config/nvim/lua/bindings.lua
+++ dotfiles/nvim/.config/nvim/lua/bindings.lua
@@ -266,6 +266,22 @@ local mappings = {
 which_key.setup(setup)
 which_key.register(mappings, opts)
 
+opts = {
+  mode = "v",    -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+mappings = {
+
+}
+
+which_key.setup(setup)
+which_key.register(mappings, opts)
+
 local K = vim.keymap.set
 K("n", "K", vim.lsp.buf.hover, {})
 
