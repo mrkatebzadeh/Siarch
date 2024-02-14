805a58fded97d327f4f9cf91fb86fecae3763c02
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-14 14:05:27 +0000

    Added sketchybar

---
+++ dotfiles/sketchybar/.config/sketchybar/items/apple.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+POPUP_OFF='sketchybar --set apple.logo popup.drawing=off'
+POPUP_CLICK_SCRIPT='sketchybar --set $NAME popup.drawing=toggle'
+
+apple_logo=(
+  icon=$APPLE
+  icon.font="$FONT:Black:16.0"
+  icon.color=$GREEN
+  padding_right=15
+  label.drawing=off
+  click_script="$POPUP_CLICK_SCRIPT"
+  popup.height=35
+)
+
+apple_prefs=(
+  icon=$PREFERENCES
+  label="Preferences"
+  click_script="open -a 'System Preferences'; $POPUP_OFF"
+)
+
+apple_activity=(
+  icon=$ACTIVITY
+  label="Activity"
+  click_script="open -a 'Activity Monitor'; $POPUP_OFF"
+)
+
+apple_logout=(
+  icon=$LOCK
+  label="Logout"
+  click_script="osascript -e 'tell app \"System Events\" to log out'; $POPUP_OFF"
+)
+
+apple_lock=(
+  icon=$LOCK
+  label="Lock Screen"
+  click_script="pmset displaysleepnow; $POPUP_OFF"
+)
+
+sketchybar --add item apple.logo left                  \
+           --set apple.logo "${apple_logo[@]}"         \
+                                                       \
+           --add item apple.prefs popup.apple.logo     \
+           --set apple.prefs "${apple_prefs[@]}"       \
+                                                       \
+           --add item apple.activity popup.apple.logo  \
+           --set apple.activity "${apple_activity[@]}" \
+                                                       \
+           --add item apple.logout popup.apple.logo  \
+           --set apple.logout "${apple_logout[@]}" \
+                                                       \
+           --add item apple.lock popup.apple.logo      \
+           --set apple.lock "${apple_lock[@]}"
---
+++ dotfiles/sketchybar/.config/sketchybar/plugins/battery.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source "$CONFIG_DIR/icons.sh"
+source "$CONFIG_DIR/colors.sh"
+
+BATTERY_INFO="$(pmset -g batt)"
+PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
+
+if [ $PERCENTAGE = "" ]; then
+  exit 0
+fi
+
+DRAWING=on
+COLOR=$WHITE
+case ${PERCENTAGE} in
+  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
+  ;;
+  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
+  ;;
+  [3-5][0-9]) ICON=$BATTERY_50 COLOR=$YELLOW
+  ;;
+  [1-2][0-9]) ICON=$BATTERY_20; COLOR=$ORANGE
+  ;;
+  *) ICON=$BATTERY_0; COLOR=$RED
+esac
+
+if [[ $CHARGING != "" ]]; then
+  ICON=$BATTERY_CHARGING
+  DRAWING=off
+fi
+DRAWING=on
+sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR 
+sketchybar --set $NAME drawing=on label="$PERCENTAGE%" label.color=$COLOR
+
+sketchybar --animate sin 20 --set $NAME 
+
+
+click() {
+  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+
+  WIDTH=0
+  if [ "$CURRENT_WIDTH" -eq "0" ]; then
+    WIDTH=dynamic
+  fi
+
+  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+}
+
+case "$SENDER" in
+  "mouse.clicked") click
+  ;;
+esac
