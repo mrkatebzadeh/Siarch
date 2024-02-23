8a5b8b98cd32c478b81558ae66a4165c021e617b
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-23 11:50:17 +0000

    Addheadergaurd added

--- dotfiles/nvim/.config/nvim/ftplugin/c.lua
+++ dotfiles/nvim/.config/nvim/ftplugin/c.lua
@@ -1,30 +1,31 @@
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
-	return
+  return
 end
 
 local opts = {
-	mode = "n", -- NORMAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
+  mode = "n",    -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
 }
 
 local mappings = {
-	k = {
-		name = "+C/C++",
-		c = {
-			name = "CMake",
-			b = { "<cmd>CMakeBuild<Cr>", "CMakeBuild" },
-			g = { "<cmd>CMakeGenerate<Cr>", "CMakeGenerate" },
-			C = { "<cmd>CMakeClean<Cr>", "CMakeClean" },
-			c = { "<cmd>CMakeClose<Cr>", "CMakeClose" },
-			o = { "<cmd>CMakeOpen<Cr>", "CMakeOpen" },
-		},
-		s = { ":ClangdSwitchSourceHeader<cr>", "Header/Src" },
-	},
+  k = {
+    name = "+C/C++",
+    c = {
+      name = "CMake",
+      b = { "<cmd>CMakeBuild<Cr>", "CMakeBuild" },
+      g = { "<cmd>CMakeGenerate<Cr>", "CMakeGenerate" },
+      C = { "<cmd>CMakeClean<Cr>", "CMakeClean" },
+      c = { "<cmd>CMakeClose<Cr>", "CMakeClose" },
+      o = { "<cmd>CMakeOpen<Cr>", "CMakeOpen" },
+    },
+    s = { ":ClangdSwitchSourceHeader<cr>", "Header/Src" },
+    h = { "<cmd>HeaderguardAdd<Cr>", "Add Headerguard" },
+  },
 }
 
 which_key.register(mappings, opts)
