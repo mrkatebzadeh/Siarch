3e914b6fe71b6d0ef9051066daa02994d53b917b
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-23 11:51:01 +0000

    Better shell

--- dotfiles/nvim/.config/nvim/lua/plugins/none-ls.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,19 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
-			},
-		})
-	end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.formatting.shfmt,
+      },
+    })
+  end,
 }
