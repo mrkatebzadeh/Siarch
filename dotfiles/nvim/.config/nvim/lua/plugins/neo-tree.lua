2d96dd8e684292273c34843e25d531b3a706030f
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-27 10:53:41 +0000

    Even better warning icon

--- dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,7 +9,7 @@ return {
   config = function()
     -- If you want icons for diagnostic errors, you'll need to define them somewhere:
     vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
     vim.fn.sign_define("DiagnosticSignInfo", { text = "󰋽 ", texthl = "DiagnosticSignInfo" })
     vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 
