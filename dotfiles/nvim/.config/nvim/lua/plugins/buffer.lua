af41a28da34286a07398df6a26a88939b68b7342
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-27 10:50:53 +0000

    Added todo, and other plugins

--- dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
@@ -100,8 +100,6 @@ return {
   {
     "hedyhli/outline.nvim",
     config = function()
-      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-
       require("outline").setup({})
     end,
   },
@@ -136,7 +134,7 @@ return {
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
 
-      require("ibl").setup({ indent = { highlight = highlight, char="│" } })
+      require("ibl").setup({ indent = { highlight = highlight, char = "│" } })
     end,
   },
   {
@@ -149,4 +147,47 @@ return {
       })
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "debugloop/telescope-undo.nvim",
+    dependencies = { -- note how they're inverted to above example
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
+    opts = {
+      -- don't use `defaults = { }` here, do this in the main telescope spec
+      extensions = {
+        undo = {
+          -- telescope-undo.nvim config, see below
+        },
+        -- no other extensions here, they can have their own spec too
+      },
+    },
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      require("telescope").setup(opts)
+      require("telescope").load_extension("undo")
+    end,
+  },
+  --[[ {
+    "sunjon/shade.nvim",
+    config = function()
+      require("shade").setup({
+        overlay_opacity = 60,
+        opacity_step = 1,
+      })
+    end,
+  }, ]]
 }
