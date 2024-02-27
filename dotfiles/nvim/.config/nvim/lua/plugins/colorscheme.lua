94093f533072a42c0e39f888f22747a294781967
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-27 10:51:16 +0000

    Let's go for frappe

--- dotfiles/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,10 +6,21 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
+				flavour = "frappe",
+				transparent_background = false,
 				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 					comments = { "italic" }, -- Change the style of comments
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
 				},
 			})
 
