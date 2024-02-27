b8703992ad32fad53dfe89d606d6964214deddd0
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-27 00:51:49 +0000

    Close neo-tree if no other window is open

--- dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -14,7 +14,7 @@ return {
 		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
 		require("neo-tree").setup({
-			close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
 			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
