fd403dabf44ec0e5263a845d90d71613390bfab7
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-02-27 02:09:38 +0000

    Better indentation indicator

--- dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
@@ -136,7 +136,7 @@ return {
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
 
-      require("ibl").setup({ indent = { highlight = highlight } })
+      require("ibl").setup({ indent = { highlight = highlight, char="│" } })
     end,
   },
   {
