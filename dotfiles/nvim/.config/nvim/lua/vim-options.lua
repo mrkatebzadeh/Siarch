9be0699b475570eff1329e56fddfb4dedd1149e9
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-03-08 16:47:56 +0000

    Added mpp

--- dotfiles/nvim/.config/nvim/lua/vim-options.lua
+++ dotfiles/nvim/.config/nvim/lua/vim-options.lua
@@ -25,3 +25,6 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
+
+
+vim.cmd [[ autocmd BufRead,BufNewFile *.mpp set filetype=mpp ]]
