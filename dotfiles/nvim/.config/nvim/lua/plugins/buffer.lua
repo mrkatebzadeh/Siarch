574105d31ca358312ad5e587a259982bdd536c12
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-03-08 20:19:34 +0000

    Removed barbar

--- dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
@@ -42,61 +42,6 @@ return {
     },
     config = true,
   },
-  --[[ {
-    "romgrk/barbar.nvim",
-    dependencies = {
-      "lewis6991/gitsigns.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-      require("barbar").setup({
-        icons = {
-          -- Configure the base icons on the bufferline.
-          -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
-          buffer_index = false,
-          buffer_number = false,
-          button = "x",
-          -- Enables / disables diagnostic symbols
-          diagnostics = {
-            [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
-            [vim.diagnostic.severity.WARN] = { enabled = false },
-            [vim.diagnostic.severity.INFO] = { enabled = false },
-            [vim.diagnostic.severity.HINT] = { enabled = true },
-          },
-          gitsigns = {
-            added = { enabled = true, icon = "+" },
-            changed = { enabled = true, icon = "~" },
-            deleted = { enabled = true, icon = "-" },
-          },
-          filetype = {
-            -- Sets the icon's highlight group.
-            -- If false, will use nvim-web-devicons colors
-            custom_colors = false,
-            -- Requires `nvim-web-devicons` if `true`
-            enabled = true,
-          },
-          separator = { left = "▎", right = "" },
-          -- If true, add an additional separator at the end of the buffer list
-          separator_at_end = true,
-          -- Configure the icons on the bufferline when modified or pinned.
-          -- Supports all the base icon options.
-          modified = { button = "●" },
-          pinned = { button = "", filename = true },
-          -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
-          preset = "default",
-          -- Configure the icons on the bufferline based on the visibility of a buffer.
-          -- Supports all the base icon options, plus `modified` and `pinned`.
-          alternate = { filetype = { enabled = false } },
-          current = { buffer_index = true },
-          inactive = { button = "×" },
-          visible = { modified = { buffer_number = false } },
-        },
-      })
-    end,
-    opts = {},
-    version = "^1.0.0",
-  }, ]]
   {
     "hedyhli/outline.nvim",
     config = function()
@@ -179,6 +124,12 @@ return {
       -- defaults, as well as each extension).
       require("telescope").setup(opts)
       require("telescope").load_extension("undo")
+    end,
+  },
+  {
+    "SmiteshP/nvim-navic",
+    config = function()
+      require("nvim-navic").setup()
     end,
   },
   --[[ {
