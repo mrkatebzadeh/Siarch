b662e0a4175fceced855d7149f10fbc17fb3fb61
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-03-08 16:37:32 +0000

    Better binding for rust

--- dotfiles/nvim/.config/nvim/ftplugin/rust.lua
+++ dotfiles/nvim/.config/nvim/ftplugin/rust.lua
@@ -4,9 +4,9 @@ if not status_ok then
 end
 
 local opts = {
-  mode = "n", -- NORMAL mode
+  mode = "n",    -- NORMAL mode
   prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
   silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
   nowait = true, -- use `nowait` when creating keymaps
@@ -14,12 +14,12 @@ local opts = {
 
 local mappings = {
   k = {
-    name = "+ Rust",
+    name = "Rust",
     a = { "<cmd>:RustLsp codeAction<Cr>", "Actions" },
     r = { "<cmd>:RustLsp runnables<Cr>", "Runnables" },
     t = { "<cmd>:RustLsp testables<cr>", "Cargo Test" },
     m = { "<cmd>:RustLsp expandMacro<Cr>", "Expand Macro" },
-    c = { "<cmd>:RustLsp openCargo<Cr>", "Open Cargo" },
+    C = { "<cmd>:RustLsp openCargo<Cr>", "Open Cargo" },
     h = { "<cmd>:RustLsp externalDocs<Cr>", "Open Docs" },
     p = { "<cmd>:RustLsp parentModule<Cr>", "Parent Module" },
     d = { "<cmd>:RustLsp debuggables<Cr>", "Debuggables" },
@@ -28,11 +28,14 @@ local mappings = {
       "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
       "Reload Workspace",
     },
-    y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
-    P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
-    i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
-    f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
-    D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+    c = {
+      name = "Crate",
+      y = { "<cmd>lua require'crates'.open_repository()<cr>", "Open Repository" },
+      p = { "<cmd>lua require'crates'.show_popup()<cr>", "Show Popup" },
+      i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "Show Info" },
+      f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "Show Features" },
+      d = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "Show Dependencies" },
+    },
   },
 }
 
