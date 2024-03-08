58db54cb4b9f1d4c5ef6ce86af63462796c39b57
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-03-08 16:41:51 +0000

    Added test to sidebars

--- dotfiles/nvim/.config/nvim/lua/bindings/o_options.lua
+++ dotfiles/nvim/.config/nvim/lua/bindings/o_options.lua
@@ -1,45 +1,54 @@
 O_OPTIONS = {}
 
 O_OPTIONS.setup = function()
-  local status_ok, which_key = pcall(require, "which-key")
-  if not status_ok then
-    return
-  end
+	local status_ok, which_key = pcall(require, "which-key")
+	if not status_ok then
+		return
+	end
+	function toggle_hints()
+		if vim.g.diagnostics_active then
+			vim.diagnostic.config({ virtual_text = false })
+		else
+			vim.diagnostic.config({ virtual_text = true })
+		end
+	end
 
-  local opts = {
-    mode = "n",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = true,
-  }
+	local opts = {
+		mode = "n",
+		prefix = "<leader>",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = true,
+	}
 
-  local mappings = {
-    o = {
-      name = "Options",
-      w = { "<cmd>set wrap!<CR>", "Soft Wrap Text" },
-      z = { "<cmd>ZenMode<CR>", "Zend Mode" },
-      c = { "<cmd>ColorizerToggle<CR>", "Toggle Colorizer" },
-      o = { "<cmd>Outline<CR>", "Toggle Outline" },
-      i = { "<cmd>IBLToggle<cr>", "Indent Hints" },
-      t = { "<cmd>TodoTelescope<cr>", "TODOs" },
-      u = { "<cmd>Telescope undo<cr>", "Undo" },
-    },
-  }
+	local mappings = {
+		o = {
+			name = "Options",
+			b = { "<cmd>lua require('edgy').toggle()<cr>", "Toggle Bars" },
+			w = { "<cmd>set wrap!<CR>", "Soft Wrap Text" },
+			z = { "<cmd>ZenMode<CR>", "Zend Mode" },
+			c = { "<cmd>ColorizerToggle<CR>", "Toggle Colorizer" },
+			h = { name = "Toggle Hints" },
+			o = { "<cmd>Outline<CR>", "Toggle Outline" },
+			i = { "<cmd>IBLToggle<cr>", "Indent Hints" },
+			t = { "<cmd>TodoTelescope<cr>", "TODOs" },
+			u = { "<cmd>Telescope undo<cr>", "Undo" },
+		},
+	}
 
-  which_key.register(mappings, opts)
+	which_key.register(mappings, opts)
 
-  opts = {
-    mode = "v",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = true,
-  }
+	opts = {
+		mode = "v",
+		prefix = "<leader>",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = true,
+	}
 
-  which_key.register(mappings, opts)
+	which_key.register(mappings, opts)
 end
 
 return O_OPTIONS
