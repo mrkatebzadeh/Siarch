d6cd2b9ae07b3ab44aaf4e219bd068d309b19db9
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-03-10 12:52:06 +0000

    Added bufferline

--- dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffer.lua
@@ -132,13 +132,34 @@ return {
       require("nvim-navic").setup()
     end,
   },
-  --[[ {
-    "sunjon/shade.nvim",
-    config = function()
-      require("shade").setup({
-        overlay_opacity = 60,
-        opacity_step = 1,
-      })
+  {
+    "akinsho/bufferline.nvim",
+    version = "v4.5.2",
+    opts = function()
+      local Offset = require("bufferline.offset")
+      if not Offset.edgy then
+        local get = Offset.get
+        Offset.get = function()
+          if package.loaded.edgy then
+            local layout = require("edgy.config").layout
+            local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+            for _, pos in ipairs({ "left", "right" }) do
+              local sb = layout[pos]
+              if sb and #sb.wins > 0 then
+                local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+                ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+                ret[pos .. "_size"] = sb.bounds.width
+              end
+            end
+            ret.total_size = ret.left_size + ret.right_size
+            if ret.total_size > 0 then
+              return ret
+            end
+          end
+          return get()
+        end
+        Offset.edgy = true
+      end
     end,
-  }, ]]
+  },
 }
