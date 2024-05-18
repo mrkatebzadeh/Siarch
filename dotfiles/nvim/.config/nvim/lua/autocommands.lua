73b2b3f035e6ca35e38c14648fc51e4995078503
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-05-18 08:35:48 +0100

    Fixed icons

--- dotfiles/nvim/.config/nvim/lua/autocommands.lua
+++ dotfiles/nvim/.config/nvim/lua/autocommands.lua
@@ -66,6 +66,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+local ns = vim.api.nvim_create_namespace("CurlineDiag")
+vim.opt.updatetime = 100
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.api.nvim_create_autocmd("CursorHold", {
+      buffer = args.buf,
+      callback = function()
+        pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
+        local hi = { "Error", "Warn", "Info", "Hint" }
+        local curline = vim.api.nvim_win_get_cursor(0)[1]
+        local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
+        local virt_texts = { { (" "):rep(4) } }
+        for _, diag in ipairs(diagnostics) do
+          virt_texts[#virt_texts + 1] = { diag.message, "Diagnostic" .. hi[diag.severity] }
+        end
+        vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
+          virt_text = virt_texts,
+          hl_mode = "combine",
+        })
+      end,
+    })
+  end,
+})
+
 vim.diagnostic.config({
   virtual_text = false,
   signs = true,
