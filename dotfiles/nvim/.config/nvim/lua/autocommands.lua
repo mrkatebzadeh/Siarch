c7453b0a64904ebdf020b2c47777025fd9faea98
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-05-27 22:18:10 +0100

    Added signature

--- dotfiles/nvim/.config/nvim/lua/autocommands.lua
+++ dotfiles/nvim/.config/nvim/lua/autocommands.lua
@@ -1,109 +1,125 @@
 --- Don't create a comment string when hitting <Enter> on a comment line
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("DisableNewLineAutoCommentString", {}),
-  callback = function()
-    vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
-  end,
+	group = vim.api.nvim_create_augroup("DisableNewLineAutoCommentString", {}),
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+	end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    local bufnr = ev.buf
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local bufnr = ev.buf
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-    vim.cmd.setlocal("signcolumn=yes")
-    vim.bo[bufnr].bufhidden = "hide"
+		vim.cmd.setlocal("signcolumn=yes")
+		vim.bo[bufnr].bufhidden = "hide"
 
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    if client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint.enable(true, {bufnr = bufnr})
-      local wk = require("which-key")
-      local opts = {
-        mode = "n",
-        prefix = "<leader>",
-        buffer = nil,
-        silent = true,
-        noremap = true,
-        nowait = true,
-      }
-      wk.register({
-        o = {
-          h = {
-            function()
-              local current_setting = vim.lsp.inlay_hint.is_enabled({bufnr = bufnr})
-              vim.lsp.inlay_hint.enable(not current_setting, {bufnr = bufnr})
-            end,
-            "Toggle Inlay Hints",
-          },
-        },
-      }, opts)
-    end
+		if client.server_capabilities.inlayHintProvider then
+			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			local wk = require("which-key")
+			local opts = {
+				mode = "n",
+				prefix = "<leader>",
+				buffer = nil,
+				silent = true,
+				noremap = true,
+				nowait = true,
+			}
+			wk.register({
+				o = {
+					h = {
+						function()
+							local current_setting = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+							vim.lsp.inlay_hint.enable(not current_setting, { bufnr = bufnr })
+						end,
+						"Toggle Inlay Hints",
+					},
+				},
+			}, opts)
+		end
 
-   --[[  -- Auto-refresh code lenses
-    if not client then
-      return
-    end
-    local function buf_refresh_codeLens()
-      vim.schedule(function()
-        if client.server_capabilities.codeLensProvider then
-          vim.lsp.codelens.refresh()
-          return
-        end
-      end)
-    end
-    local group = vim.api.nvim_create_augroup(string.format("lsp-%s-%s", bufnr, client.id), {})
-    if client.server_capabilities.codeLensProvider then
-      vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost", "TextChanged" }, {
-        group = group,
-        callback = buf_refresh_codeLens,
-        buffer = bufnr,
-      })
-      buf_refresh_codeLens()
-    end ]]
-  end,
+		-- Auto-refresh code lenses
+		if not client then
+			return
+		end
+		local function buf_refresh_codeLens()
+			vim.schedule(function()
+				if client.server_capabilities.codeLensProvider then
+					vim.lsp.codelens.refresh()
+					return
+				end
+			end)
+		end
+		local group = vim.api.nvim_create_augroup(string.format("lsp-%s-%s", bufnr, client.id), {})
+		if client.server_capabilities.codeLensProvider then
+			vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost", "TextChanged" }, {
+				group = group,
+				callback = buf_refresh_codeLens,
+				buffer = bufnr,
+			})
+			buf_refresh_codeLens()
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if vim.tbl_contains({ "null-ls" }, client.name) then -- blacklist lsp
+			return
+		end
+		require("lsp_signature").on_attach({
+			bind = true, -- This is mandatory, otherwise border config won't get registered.
+			handler_opts = {
+				border = "rounded",
+			},
+		}, bufnr)
+	end,
 })
 
 local ns = vim.api.nvim_create_namespace("CurlineDiag")
 vim.opt.updatetime = 100
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    vim.api.nvim_create_autocmd("CursorHold", {
-      buffer = args.buf,
-      callback = function()
-        pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
-        local hi = { "Error", "Warn", "Info", "Hint" }
-        local curline = vim.api.nvim_win_get_cursor(0)[1]
-        local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
-        local virt_texts = { { (" "):rep(4) } }
-        for _, diag in ipairs(diagnostics) do
-          virt_texts[#virt_texts + 1] = { diag.message, "Diagnostic" .. hi[diag.severity] }
-        end
-        vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
-          virt_text = virt_texts,
-          hl_mode = "combine",
-        })
-      end,
-    })
-  end,
+	callback = function(args)
+		vim.api.nvim_create_autocmd("CursorHold", {
+			buffer = args.buf,
+			callback = function()
+				pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
+				local hi = { "Error", "Warn", "Info", "Hint" }
+				local curline = vim.api.nvim_win_get_cursor(0)[1]
+				local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
+				local virt_texts = { { (" "):rep(4) } }
+				for _, diag in ipairs(diagnostics) do
+					virt_texts[#virt_texts + 1] = { diag.message, "Diagnostic" .. hi[diag.severity] }
+				end
+				vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
+					virt_text = virt_texts,
+					hl_mode = "combine",
+				})
+			end,
+		})
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  float = {
-    border = "single",
-    format = function(diagnostic)
-      return string.format(
-        "%s (%s) [%s]",
-        diagnostic.message,
-        diagnostic.source,
-        diagnostic.code or diagnostic.user_data.lsp.code
-      )
-    end,
-  },
+	virtual_text = false,
+	signs = true,
+	float = {
+		border = "single",
+		format = function(diagnostic)
+			return string.format(
+				"%s (%s) [%s]",
+				diagnostic.message,
+				diagnostic.source,
+				diagnostic.code or diagnostic.user_data.lsp.code
+			)
+		end,
+	},
 })
 
 local icons = require("icons")
@@ -120,13 +136,13 @@ vim.fn.sign_define("DapLogPoint", { text = icons.DapLogPoint, texthl = "Diagnost
 vim.fn.sign_define("DapStopped", { text = icons.DapStopped, texthl = "DiagnosticWarn" })
 
 vim.diagnostic.config({
-  signs = {
-    --support diagnostic severity / diagnostic type name
-    text = {
-      [vim.diagnostic.severity.ERROR] = icons.DiagnosticError,
-      [vim.diagnostic.severity.WARN] = icons.DiagnosticWarn,
-      [vim.diagnostic.severity.INFO] = icons.DiagnosticInfo,
-      [vim.diagnostic.severity.HINT] = icons.DiagnosticHint,
-    },
-  },
+	signs = {
+		--support diagnostic severity / diagnostic type name
+		text = {
+			[vim.diagnostic.severity.ERROR] = icons.DiagnosticError,
+			[vim.diagnostic.severity.WARN] = icons.DiagnosticWarn,
+			[vim.diagnostic.severity.INFO] = icons.DiagnosticInfo,
+			[vim.diagnostic.severity.HINT] = icons.DiagnosticHint,
+		},
+	},
 })
--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,14 +14,34 @@ return {
 			auto_install = true,
 		},
 	},
-	--[[ {
-    "ray-x/lsp_signature.nvim",
-    event = "VeryLazy",
-    opts = {},
-    config = function(_, opts)
-      require("lsp_signature").setup(opts)
-    end,
-  }, ]]
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			local cfg = {
+				floating_window_off_x = 5, -- adjust float windows x position.
+				floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+					local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+					local pumheight = vim.o.pumheight
+					local winline = vim.fn.winline() -- line number in the window
+					local winheight = vim.fn.winheight(0)
+
+					-- window top
+					if winline - 1 < pumheight then
+						return pumheight
+					end
+
+					-- window bottom
+					if winheight - winline < pumheight then
+						return -pumheight
+					end
+					return 0
+				end,
+			}
+			require("lsp_signature").setup(cfg)
+		end,
+	},
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
@@ -39,7 +59,7 @@ return {
 			lspconfig.texlab.setup({
 				capabilities = capabilities,
 			})
-      lspconfig.nil_ls.setup({
+			lspconfig.nil_ls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.tsserver.setup({
@@ -123,8 +143,8 @@ return {
 		"rmagatti/goto-preview",
 		config = function()
 			require("goto-preview").setup({
-        border = {"╭", "─" ,"╮", "│", "╯", "─", "╰", "│"};
-      })
+				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+			})
 		end,
 	},
 }
