b1ada0b92900e269e6a45a473c3d467ea3943d3c
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-04 00:24:13 +0100

    Added rebuild

---
+++ rebuild.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+install_nix() {
+	echo "Installing Nix..."
+	curl -L https://nixos.org/nix/install | sh -s -- --daemon --yes
+	if [ $? -ne 0 ]; then
+		echo "Nix installation failed!"
+		exit 1
+	fi
+
+	. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+
+	echo "Configuring Nix..."
+	mkdir -p $HOME/.config/nix
+	echo "experimental-features = nix-command flakes" >$HOME/.config/nix/nix.conf
+
+	echo "Nix installed and configured with nix-command and flakes features."
+}
+
+check_nix_installed() {
+	if command -v nix >/dev/null 2>&1; then
+		echo "Nix is already installed."
+		return 0
+	else
+		return 1
+	fi
+}
+
+install_home_manager() {
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		echo "Installing Home Manager..."
+		nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+		nix-channel --update
+		nix-shell '<home-manager>' -A install
+		if [ $? -ne 0 ]; then
+			echo "Home Manager installation failed!"
+			exit 1
+		fi
+
+		echo "Home Manager installed successfully."
+	fi
+}
+
+check_home_manager_installed() {
+	if command -v home-manager >/dev/null 2>&1; then
+		echo "Home Manager is already installed."
+		return 0
+	else
+		return 1
+	fi
+}
+
+rebuild_configuration() {
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		echo "Rebuilding configuration with Nix on Linux..."
+		nix run home-manager -- switch --flake .
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "Rebuilding configuration with Nix on macOS..."
+		nix run nix-darwin -- switch --flake .
+	else
+		echo "Unsupported OS: $OSTYPE"
+		exit 1
+	fi
+}
+
+check_nix_installed || install_nix
+
+check_home_manager_installed || install_home_manager
+
+rebuild_configuration
