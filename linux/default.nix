23b87719063d2a0a8c6a0ce87072d19df3462b93
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-03 01:18:26 +0100

    Added linux packages

--- linux/default.nix
+++ linux/default.nix
@@ -7,39 +7,88 @@
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
-
+    abook
     asdf-vm
+    atool
+    bc
+    brave
     btop
     curl
+    dmenu
+    dosfstools
+    dunst
+    dwm
+    dwmblocks
     emacs
+    exfat
     fd
+    ffmpeg
     fontconfig
     fzf
     gawk
     git
     git-filter-repo
+    gnome.gnome-keyring
     gpg-tui
+    highlight
     htop
     jadx
     kitty
     lazydocker
     lazygit
     less
+    lf
+    libnotify
     lsd
+    lynx
+    maim
+    man
+    mediainfo
+    mpc-cli
+    mpd
+    mpv
+    mutt-wizard
+    ncmpcpp
+    newsboat
     nix-search-cli
+    ntfs3g
     nushell
+    poppler
+    pulseaudio
+    pulsemixer
     python311Packages.pynvim
     ripgrep
+    sc-im
+    simple-mtpfs
     slack
+    slock
+    st
     starship
     stow
     stylua
+    sxiv
+    taskspooler
     telegram-desktop
     tree-sitter
+    unclutter
+    unrar
     unstable.neovim
+    unzip
     vim
     wget
+    xcape
+    xdotool
+    xorg.xbacklight
+    xorg.xdpyinfo
+    xorg.xinit
+    xorg.xorgserver
+    xorg.xprop
+    xorg.xwininfo
+    xwallpaper
+    youtube-dl
+    zathura
     zellij
+    zsh
   ];
 
   home.file = { };
