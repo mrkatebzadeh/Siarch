19ba0c8495643adb911d0485ed6abb1fc2eba875
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-06 01:45:49 +0100

    Use rofi

---
+++ home/nixfiles/kitty.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  font = { package = pkgs.jetbrains-mono; name = "JetBrains Mono"; };
+  settings = {
+    font_size = 14;
+    background = "#303446";
+    foreground = "#C6D0F5";
+    color0 = "#51576D";
+    color8 = "#626880";
+    color1 = "#E78284";
+    color9 = "#E78284";
+    color2 = "#A6D189";
+    color10 = "#A6D189";
+    color3 = "#E5C890";
+    color11 = "#E5C890";
+    color4 = "#8CAAEE";
+    color12 = "#8CAAEE";
+    color5 = "#F4B8E4";
+    color13 = "#F4B8E4";
+    color6 = "#81C8BE";
+    color14 = "#81C8BE";
+    color7 = "#B5BFE2";
+    color15 = "#A5ADCE";
+    term = "xterm-256color";
+    window_padding_width = 1;
+    window_margin_width = 6;
+    window_border_width = 1;
+
+  };
+}
