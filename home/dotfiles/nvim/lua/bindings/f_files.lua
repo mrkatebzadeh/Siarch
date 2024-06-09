1a44935f2bb842675e88aec822dd4aecf4a54e00
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-09 17:17:25 +0100

    Added telescope file_browser

--- home/dotfiles/nvim/lua/bindings/f_files.lua
+++ home/dotfiles/nvim/lua/bindings/f_files.lua
@@ -1,53 +1,54 @@
 F_FILES = {}
 
 F_FILES.setup = function()
-  local status_ok, which_key = pcall(require, "which-key")
-  if not status_ok then
-    return
-  end
+	local status_ok, which_key = pcall(require, "which-key")
+	if not status_ok then
+		return
+	end
 
-  local opts = {
-    mode = "n",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = true,
-  }
+	local opts = {
+		mode = "n",
+		prefix = "<leader>",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = true,
+	}
 
-  local mappings = {
-    f = {
-      name = "Files",
-      n = { "<cmd>Neotree toggle filesystem<cr>", "NeoTree" },
-      b = { "<cmd>Neotree buffers reveal float<cr>", "Buffers" },
-      f = { "<cmd>Telescope find_files<cr>", "Find File" },
-      g = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-      o = { "<cmd>Telescope oldfiles theme=ivy<cr>", "Old Files" },
-      h = { "<cmd>Telescope help_tags theme=ivy<cr>", "Help Tags" },
-      e = { ":e ", "e" },
-      m = { ":Move ", "Move" },
-      d = { ":Delete<CR>", "Delete" },
-      r = { ":Rename ", "Rename" },
-      c = { ":Chmod ", "Chmod" },
-      k = { ":Mkdir ", "Mkdir" },
-      w = { ":cmd>w!<CR>", "Save" },
-      W = { ":SudoWrite<CR>", "SudoWrite" },
-      E = { ":SudoEdit ", "SudoEdit" },
-    },
-  }
+	local mappings = {
+		f = {
+			name = "Files",
+			n = { "<cmd>Telescope file_browser<cr>", "File Browser" },
+			N = { "<cmd>Neotree toggle filesystem<cr>", "NeoTree" },
+			b = { "<cmd>Neotree buffers reveal float<cr>", "Buffers" },
+			f = { "<cmd>Telescope find_files<cr>", "Find File" },
+			g = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+			o = { "<cmd>Telescope oldfiles theme=ivy<cr>", "Old Files" },
+			h = { "<cmd>Telescope help_tags theme=ivy<cr>", "Help Tags" },
+			e = { ":e ", "e" },
+			m = { ":Move ", "Move" },
+			d = { ":Delete<CR>", "Delete" },
+			r = { ":Rename ", "Rename" },
+			c = { ":Chmod ", "Chmod" },
+			k = { ":Mkdir ", "Mkdir" },
+			w = { ":cmd>w!<CR>", "Save" },
+			W = { ":SudoWrite<CR>", "SudoWrite" },
+			E = { ":SudoEdit ", "SudoEdit" },
+		},
+	}
 
-  which_key.register(mappings, opts)
+	which_key.register(mappings, opts)
 
-  opts = {
-    mode = "v",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = true,
-  }
+	opts = {
+		mode = "v",
+		prefix = "<leader>",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = true,
+	}
 
-  which_key.register(mappings, opts)
+	which_key.register(mappings, opts)
 end
 
 return F_FILES
--- home/dotfiles/nvim/lua/plugins/telescope.lua
+++ home/dotfiles/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,9 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
 		config = function()
 			require("telescope").setup({
 				extensions = {
@@ -17,5 +19,12 @@ return {
 
 			require("telescope").load_extension("ui-select")
 		end,
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
 	},
 }
