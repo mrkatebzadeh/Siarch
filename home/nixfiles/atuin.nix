0a0c2412265db03736708f08a572af102b19c403
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-09 13:44:08 +0100

    Fixed up arrow

--- home/nixfiles/atuin.nix
+++ home/nixfiles/atuin.nix
@@ -5,7 +5,7 @@
     enter_accept = false;
     prefers_reduced_motion = true;
     filter_mode = "host";
-    invert = true;
+    invert = false;
     inline_height = 10;
     style = "compact";
     show_help = false;
