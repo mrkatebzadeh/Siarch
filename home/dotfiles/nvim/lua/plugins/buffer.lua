56e0110fa792d4cfb98f673f2435ec5006d58e89
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-14 09:47:48 +0100

    Removed tab

--- home/dotfiles/nvim/lua/plugins/buffer.lua
+++ home/dotfiles/nvim/lua/plugins/buffer.lua
@@ -1,184 +1,159 @@
 return {
-  {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true,
-  },
-  {
-    "RRethy/vim-illuminate",
-    lazy = true,
-    event = "BufReadPost",
-    opts = {
-      filetypes_denylist = {
-        "NvimTree",
-      },
-    },
-    config = function(_, opts)
-      require("illuminate").configure(opts)
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+	},
+	{
+		"RRethy/vim-illuminate",
+		lazy = true,
+		event = "BufReadPost",
+		opts = {
+			filetypes_denylist = {
+				"NvimTree",
+			},
+		},
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
-      local highlight = "#354A51"
-      vim.api.nvim_set_hl(0, "IlluminatedWordText", {
-        bg = highlight,
-      })
-      vim.api.nvim_set_hl(0, "IlluminatedWordRead", {
-        bg = highlight,
-      })
-      vim.api.nvim_set_hl(0, "IlluminatedWordWrite", {
-        bg = highlight,
-      })
-    end,
-  },
-  {
-    "cappyzawa/trim.nvim",
-    opts = {},
-  },
-  {
-    "utilyre/barbecue.nvim",
-    version = "v1.*",
-    lazy = true,
-    event = "BufReadPre",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = true,
-  },
-  {
-    "hedyhli/outline.nvim",
-    config = function()
-      require("outline").setup({})
-    end,
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-  },
-  { "onsails/lspkind.nvim" },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
-      }
+			local highlight = "#51576d"
+			vim.api.nvim_set_hl(0, "IlluminatedWordText", {
+				bg = highlight,
+			})
+			vim.api.nvim_set_hl(0, "IlluminatedWordRead", {
+				bg = highlight,
+			})
+			vim.api.nvim_set_hl(0, "IlluminatedWordWrite", {
+				bg = highlight,
+			})
+		end,
+	},
+	{
+		"cappyzawa/trim.nvim",
+		opts = {},
+	},
+	{
+		"utilyre/barbecue.nvim",
+		version = "v1.*",
+		lazy = true,
+		event = "BufReadPre",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = true,
+	},
+	{
+		"hedyhli/outline.nvim",
+		config = function()
+			require("outline").setup({})
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+	},
+	{ "onsails/lspkind.nvim" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
 
-      local hooks = require("ibl.hooks")
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      end)
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#e78284" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#e5c890" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#8caaee" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ef9f76" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#a6d189" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#ca9ee6" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#85c1dc" })
+			end)
 
-      require("ibl").setup({ indent = { highlight = highlight, char = "│" } })
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-  {
-    "debugloop/telescope-undo.nvim",
-    dependencies = { -- note how they're inverted to above example
-      {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-      },
-    },
-    opts = {
-      -- don't use `defaults = { }` here, do this in the main telescope spec
-      extensions = {
-        undo = {
-          -- telescope-undo.nvim config, see below
-        },
-        -- no other extensions here, they can have their own spec too
-      },
-    },
-    config = function(_, opts)
-      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-      -- defaults, as well as each extension).
-      require("telescope").setup(opts)
-      require("telescope").load_extension("undo")
-    end,
-  },
-  {
-    "SmiteshP/nvim-navic",
-    config = function()
-      require("nvim-navic").setup()
-    end,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    version = "v4.6",
-    opts = function()
-      local Offset = require("bufferline.offset")
-      if not Offset.edgy then
-        local get = Offset.get
-        Offset.get = function()
-          if package.loaded.edgy then
-            local layout = require("edgy.config").layout
-            local ret = { left = "", left_size = 0, right = "", right_size = 0 }
-            for _, pos in ipairs({ "left", "right" }) do
-              local sb = layout[pos]
-              if sb and #sb.wins > 0 then
-                local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
-                ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
-                ret[pos .. "_size"] = sb.bounds.width
-              end
-            end
-            ret.total_size = ret.left_size + ret.right_size
-            if ret.total_size > 0 then
-              return ret
-            end
-          end
-          return get()
-        end
-        Offset.edgy = true
-      end
-    end,
-  },
-  {
-    "j-hui/fidget.nvim",
-    tag = "v1.4.5",
-    opts = {},
-  },
-  {
-    "lewis6991/satellite.nvim",
-    tag = "v1.0.0",
-    config = function()
-      require("satellite").setup({})
-    end,
-  },
-  {
-    "chentoast/marks.nvim",
-    commit = "74e8d01",
-    config = function()
-      require("marks").setup({})
-    end,
-  },
+			require("ibl").setup({ indent = { highlight = highlight, char = "│" } })
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		opts = {
+			extensions = {
+				undo = {},
+			},
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+    enabled = false,
+		version = "v4.6",
+		opts = function()
+			local Offset = require("bufferline.offset")
+			if not Offset.edgy then
+				local get = Offset.get
+				Offset.get = function()
+					if package.loaded.edgy then
+						local layout = require("edgy.config").layout
+						local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+						for _, pos in ipairs({ "left", "right" }) do
+							local sb = layout[pos]
+							if sb and #sb.wins > 0 then
+								local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+								ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+								ret[pos .. "_size"] = sb.bounds.width
+							end
+						end
+						ret.total_size = ret.left_size + ret.right_size
+						if ret.total_size > 0 then
+							return ret
+						end
+					end
+					return get()
+				end
+				Offset.edgy = true
+			end
+		end,
+	},
+	{
+		"lewis6991/satellite.nvim",
+		tag = "v1.0.0",
+		config = function()
+			require("satellite").setup({})
+		end,
+	},
+	{
+		"chentoast/marks.nvim",
+		commit = "74e8d01",
+		config = function()
+			require("marks").setup({})
+		end,
+	},
 }
