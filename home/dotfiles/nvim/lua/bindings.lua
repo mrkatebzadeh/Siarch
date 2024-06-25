7531d16c2d455c57be1e6864decc2534b837e5ca
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-06-25 14:44:37 +0100

    Cleaned

--- home/dotfiles/nvim/lua/bindings.lua
+++ home/dotfiles/nvim/lua/bindings.lua
@@ -24,27 +24,22 @@ end
 
 local setup = {
   plugins = {
-    marks = true,    -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    marks = true,
+    registers = true,
     spelling = {
-      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
+      enabled = true,
+      suggestions = 20,
     },
-    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-    -- No actual key bindings are created
     presets = {
-      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = true,   -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
-      windows = true,   -- default bindings on <c-w>
-      nav = true,       -- misc bindings to work with windows
-      z = true,         -- bindings for folds, spelling and others prefixed with z
-      g = true,         -- bindings for prefixed with g
+      operators = false,
+      motions = true,
+      text_objects = true,
+      windows = true,
+      nav = true,
+      z = true,
+      g = true,
     },
   },
-  -- add operators that will trigger motion and text object completion
-  -- to enable all native operators, set the preset / operators plugin above
-  -- operators = { gc = "Comments" },
   key_labels = {
     -- override the label used to display some keys. It doesn't effect WK in any other way.
     -- For example:
@@ -53,48 +48,44 @@ local setup = {
     -- ["<tab>"] = "TAB",
   },
   icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+",    -- symbol prepended to a group
+    breadcrumb = "»",
+    separator = "➜",
+    group = "+",
   },
   popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+    scroll_down = "<c-d>",
+    scroll_up = "<c-u>",
   },
   window = {
-    border = "rounded",     -- none, single, double, shadow
-    position = "bottom",    -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    border = "rounded",
+    position = "bottom",
+    margin = { 1, 0, 1, 0 },
+    padding = { 2, 2, 2, 2 },
     winblend = 0,
   },
   layout = {
-    height = { min = 4, max = 25 },                                            -- min and max height of the columns
-    width = { min = 20, max = 50 },                                            -- min and max width of the columns
-    spacing = 3,                                                               -- spacing between columns
-    align = "left",                                                            -- align columns left, center or right
+    height = { min = 4, max = 25 },
+    width = { min = 20, max = 50 },
+    spacing = 3,
+    align = "left",
   },
-  ignore_missing = true,                                                       -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = true,                                                            -- show help message on the command line when the popup is visible
-  triggers = "auto",                                                           -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
+  ignore_missing = true,
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
+  show_help = true,
+  triggers = "auto",
   triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
     i = { "j", "k" },
     v = { "j", "k" },
   },
 }
 
 local opts = {
-  mode = "n",    -- NORMAL mode
+  mode = "n",
   prefix = "<leader>",
-  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
+  buffer = nil,
+  silent = true,
+  noremap = true,
+  nowait = true,
 }
 
 local mappings = {
