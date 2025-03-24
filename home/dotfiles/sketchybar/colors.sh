665026b395539a3858a4a70e02e6040f3da6d9d5
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-03-24 02:01:27 +0000

    Cleaner design

--- home/dotfiles/sketchybar/colors.sh
+++ home/dotfiles/sketchybar/colors.sh
@@ -26,7 +26,7 @@ export ORANGE=0xfff5a97f
 export MAGENTA=0xffc6a0f6
 export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
-export BG0=0xff232634
+export BG0=0xaa232634
 export BG1=0x603c3e4f
 export BG2=0xff626880
 
@@ -35,7 +35,7 @@ export BAR_COLOR=$BG0
 export BAR_BORDER_COLOR=$BG2
 export BACKGROUND_1=$BG1
 export BACKGROUND_2=$BG2
-export ICON_COLOR=$WHITE # Color of all icons
+export ICON_COLOR=$WHITE  # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
 export POPUP_BACKGROUND_COLOR=$BAR_COLOR
 export POPUP_BORDER_COLOR=$WHITE
